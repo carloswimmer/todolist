7382af3489dd27e1ad0279a4ab8376629ff7dfc4
Carlos Wimmer <coiote777@gmail.com>
2026-01-04 07:43:45 +0100

    Checking controller

--- src/main/java/com/carloswimmer/todolist/controller/HomeController.java
+++ src/main/java/com/carloswimmer/todolist/controller/HomeController.java
@@ -1,16 +1,15 @@
 package com.carloswimmer.todolist.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/health")
 public class HomeController {
-
-	@GetMapping("/")
-	@ResponseBody
-	public String home() {
-		return "Welcome to TodoList API!";
-	}
+    
+    @GetMapping("/")
+    public String checkHealth() {
+        return "It is working!";
+    }
 }
-
