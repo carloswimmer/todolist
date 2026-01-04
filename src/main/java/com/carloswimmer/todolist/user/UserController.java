df82f97a32afab09a5c3f1909aee52e6182d0f5c
Carlos Wimmer <coiote777@gmail.com>
2026-01-04 09:29:20 +0100

    Install Lombok for getters and setters creation

--- src/main/java/com/carloswimmer/todolist/user/UserController.java
+++ src/main/java/com/carloswimmer/todolist/user/UserController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
+
     @PostMapping("/")
     public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getName());
+        System.out.println(userModel.getUsername());
     }
 }
--- src/main/java/com/carloswimmer/todolist/user/UserModel.java
+++ src/main/java/com/carloswimmer/todolist/user/UserModel.java
@@ -1,32 +1,11 @@
 package com.carloswimmer.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
 
     private String username;
     private String name;
     private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
